6f43acbb00a91f7e739a6430faf5da6edd40a114
Andy Reagan <areagan@massmutual.com>
2024-05-09 16:06:59 -0400

    Updated to use extended tables to reduce redundancy.

--- basic_quereies.sql
+++ basic_quereies.sql
@@ -1,253 +1,271 @@
+CREATE TABLE Inc_main_extended AS
+SELECT
+    Inc_main.*,
+    -- pull in longer description
+    lookup.Descript,
+    -- make a collapsed description
+    CASE
+        WHEN Inci_type <=299 THEN 'Fire General' 
+        WHEN Inci_type = 321 THEN 'EMS' 
+        WHEN Inci_type in (322, 323, 324) THEN 'Motor Vehicle' 
+        WHEN Inci_type >= 300 AND Inci_type <= 399 THEN 'Rescue' 
+        WHEN Inci_type >= 400 AND Inci_type <= 499 THEN 'Hazardous Condition (no fire)' 
+        WHEN Inci_type >= 800 AND Inci_type <= 899 THEN 'Hazardous Condition (no fire)' 
+        WHEN Inci_type >= 500 AND Inci_type <= 599 THEN 'Service Call' 
+        WHEN Inci_type >= 600 AND Inci_type <= 699 THEN 'Good Intent' 
+        WHEN Inci_type >= 700 AND Inci_type <= 799 THEN 'False Alarm' 
+        ELSE Inci_type::VARCHAR 
+        END AS Inci_collapsed,  
+    -- computed times AND response numbers:
+    ((Inc_main.Arv_date + Inc_main.Arv_time) - (Inc_main.Alm_date + Inc_main.Alm_time)) AS Resp_time, 
+    (Inc_main.Per_supp + Inc_main.Per_ems + Inc_main.Per_rescue + Inc_main.Per_other) AS NumRespondingBase, 
+    coalesce(Act_det_station.responding, 0) AS NumRespondingPersonnel,
+    coalesce(Act_det.responding, 0) AS NumRespondingPersonnelScene,
+    -- was the call in town?
+    Inc_main.Mutl_aid in ('N', '1', '2') AS In_town,
+    Act_main.Activ_id
+FROM Inc_main 
+LEFT JOIN 
+    (
+        SELECT  * FROM Lkp_inci WHERE Category = 'INCI TYPE' AND code != 'UUU'
+    ) lookup 
+    ON Inc_main.Inci_type = lookup.code 
+LEFT JOIN 
+    Act_main 
+    ON Inc_main.Inci_no = Act_main.Inci_no
+LEFT JOIN 
+    (
+        SELECT  
+            Activ_id, 
+            count(*) AS responding
+        FROM 
+            Act_det
+        GROUP BY 1
+    ) Act_det_station
+    ON Act_main.Activ_id = Act_det_station.Activ_id
+LEFT JOIN 
+    (
+        SELECT  
+            Activ_id, 
+            count(*) AS responding
+        FROM 
+            Act_det 
+        WHERE 
+            Unit != 'STAT' OR Unit IS NULL
+        GROUP BY 1
+    ) Act_det 
+    ON Act_main.Activ_id = Act_det.Activ_id    
+;
+
 -- calls by incident type, width description
 
-select 
+SELECT  
     Inci_type, 
-    lookup.Descript, 
-    avg(date_part('minutes', (Arv_date + Arv_time) - (Alm_date + Alm_time))) as Resp_time, 
-    avg(Per_supp + Per_ems + Per_rescue + Per_other) as Responding, 
-    count(*) as Num_calls 
-from 
-    Inc_main 
-left join 
-    (
-        select * from Lkp_inci where Category = 'INCI TYPE' and code != 'UUU'
-    ) lookup 
-    on Inc_main.Inci_type = lookup.code 
-where 
-    year(Alm_date) in (2020, 2021, 2022, 2023) 
-group by 1, 2 
-order by 1;
+    Descript, 
+    round(avg(date_part('minutes', Resp_time))*10)/10 AS Resp_time, 
+    round(avg(NumRespondingBase)*10)/10 AS Responding, 
+    count(*) AS Num_calls 
+FROM 
+    Inc_main_extended 
+WHERE 
+    YEAR(Alm_date) IN (2020, 2021, 2022, 2023) 
+GROUP BY 1, 2 
+ORDER BY 1;
 
 -- collapsed codes
 -- in town (not responding for mutual aid)
 
-select 
-    case 
-        when Inci_type <=299 then 'Fire General'
-        when Inci_type = 321 then 'EMS'
-        when Inci_type in (322, 323, 324) then 'Motor Vehicle'
-        when Inci_type >= 300 and Inci_type <= 399 then 'Rescue'
-        when Inci_type >= 400 and Inci_type <= 499 then 'Hazardous Condition (no fire)'
-        when Inci_type >= 800 and Inci_type <= 899 then 'Hazardous Condition (no fire)'
-        when Inci_type >= 500 and Inci_type <= 599 then 'Service Call'
-        when Inci_type >= 600 and Inci_type <= 699 then 'Good Intent'
-        when Inci_type >= 700 and Inci_type <= 799 then 'False Alarm'
-        else Inci_type::varchar
-        end as Inci_collapsed, 
-    round(avg(date_part('minutes', (Arv_date + Arv_time) - (Alm_date + Alm_time)))*10)/10 as Resp_time, 
-    round(avg(Per_supp + Per_ems + Per_rescue + Per_other)*10)/10 as Responding, 
-    count(*) as Num_calls 
-from Inc_main 
-where 
-    year(alm_date) >= 2020 
-    and year(alm_date) <= 2023 
-    and Mutl_aid in ('N', '1', '2')
-group by 1
-order by 1;
+SELECT
+    Inci_collapsed, 
+    round(avg(date_part('minutes', Resp_time))*10)/10 AS Resp_time, 
+    round(avg(NumRespondingBase)*10)/10 AS Responding,  
+    count(*) AS Num_calls 
+FROM Inc_main_extended 
+WHERE 
+    YEAR(alm_date) >= 2020 
+    AND YEAR(alm_date) <= 2023 
+    AND In_town
+GROUP BY 1
+ORDER BY 1;
 
 -- response time for mutual aid
 
-select 
-    case 
-        when Inci_type <=299 then 'Fire General'
-        when Inci_type = 321 then 'EMS'
-        when Inci_type in (322, 323, 324) then 'Motor Vehicle'
-        when Inci_type >= 300 and Inci_type <= 399 then 'Rescue'
-        when Inci_type >= 400 and Inci_type <= 499 then 'Hazardous Condition (no fire)'
-        when Inci_type >= 800 and Inci_type <= 899 then 'Hazardous Condition (no fire)'
-        when Inci_type >= 500 and Inci_type <= 599 then 'Service Call'
-        when Inci_type >= 600 and Inci_type <= 699 then 'Good Intent'
-        when Inci_type >= 700 and Inci_type <= 799 then 'False Alarm'
-        else Inci_type::varchar
-        end as Inci_collapsed, 
-    round(avg(date_part('minutes', (Arv_date + Arv_time) - (Alm_date + Alm_time)))*10)/10 as Resp_time, 
-    round(avg(Per_supp + Per_ems + Per_rescue + Per_other)*10)/10 as Responding, 
-    count(*) as Num_calls 
-from 
-    Inc_main 
-where 
-    year(alm_date) >= 2020 
-    and year(alm_date) <= 2023 
-    and Mutl_aid in ('3', '4', '5')
-group by 1
-order by 1;
+SELECT  
+    Inci_collapsed, 
+    round(avg(date_part('minutes', Resp_time))*10)/10 AS Resp_time, 
+    round(avg(NumRespondingBase)*10)/10 AS Responding,  
+    count(*) AS Num_calls 
+FROM Inc_main_extended 
+WHERE 
+    YEAR(alm_date) >= 2020 
+    AND YEAR(alm_date) <= 2023 
+    AND NOT In_town
+GROUP BY 1
+ORDER BY 1;
 
 -- number responding, total:
 
-select 
-    (Per_supp + Per_ems + Per_rescue + Per_other) as responding,
-    count(*) as Num_calls 
-from 
-    Inc_main 
-where 
-    year(alm_date) >= 2020 and year(alm_date) <= 2023
-group by 1
-order by 1;
+SELECT  
+    NumRespondingBase,
+    count(*) AS Num_calls 
+FROM 
+    Inc_main_extended Inc_main
+WHERE 
+    YEAR(alm_date) >= 2020 AND YEAR(alm_date) <= 2023
+GROUP BY 1
+ORDER BY 1;
 
--- redo that with a join to the responding parties:
+-- compare the responding counts
 
-select 
+SELECT  
     Inc_main.Inci_no,
-    (Per_supp + Per_ems + Per_rescue + Per_other) as responding_inc,
-    Act_main.Activ_id,
-    Act_det.responding,
-    responding_inc=responding as match
-from 
-    Inc_main 
-left join 
-    Act_main 
-    on Inc_main.Inci_no = Act_main.Inci_no
-left join
-    (
-        select 
-            Activ_id, 
-            count(*) as responding
-        from Act_det 
-        group by 1
-    ) Act_det
-    on Act_main.Activ_id = Act_det.Activ_id
-where 
-    year(Inc_main.alm_date) >= 2020 
-    and year(Inc_main.alm_date) <= 2023 
-    and not match
+    NumRespondingBase,
+    NumRespondingPersonnel,
+    NumRespondingBase=NumRespondingPersonnel AS match
+FROM 
+    Inc_main_extended Inc_main
+WHERE 
+    YEAR(Inc_main.alm_date) >= 2020 
+    AND YEAR(Inc_main.alm_date) <= 2023 
+    AND NOT match
 order by 1;
 
--- ^ same except for 2 calls which go from 3 -> 4 responding.
--- updated:
+-- ^ same except for 2 calls which go FROM 3 -> 4 responding.
+-- number responding, not including to station:
 
-select 
-    coalesce(Act_det.responding, 0),
-    count(*) as Num_calls
-from 
-    Inc_main
-left join 
-    Act_main 
-    on Inc_main.Inci_no = Act_main.Inci_no
-left join 
-    (
-        select 
-            Activ_id, 
-            count(*) as responding
-        from 
-            Act_det 
-        where 
-            Unit != 'STAT' or Unit is null
-        group by 1
-    ) Act_det 
-    on Act_main.Activ_id = Act_det.Activ_id
-where 
-    year(Inc_main.alm_date) >= 2020 
-    and year(Inc_main.alm_date) <= 2023 
-group by 1
-order by 1;
+SELECT  
+    NumRespondingPersonnelScene,
+    count(*) AS Num_calls
+FROM 
+    Inc_main_extended Inc_main
+WHERE 
+    YEAR(Inc_main.alm_date) >= 2020 
+    AND YEAR(Inc_main.alm_date) <= 2023 
+GROUP BY 1
+ORDER BY 1;
 
 -- no response calls:
 
-select 
+SELECT  
     Inc_main.Inci_no,
-    coalesce(Act_det.responding, 0)
-from 
-    Inc_main
-left join 
-    Act_main 
-    on Inc_main.Inci_no = Act_main.Inci_no
-left join 
-    (
-        select 
-            Activ_id, 
-            count(*) as responding
-        from Act_det 
-        where Unit != 'STAT' or Unit is null
-        group by 1
-    ) Act_det 
-    on Act_main.Activ_id = Act_det.Activ_id
-where 
-    year(Inc_main.alm_date) >= 2020 
-    and year(Inc_main.alm_date) <= 2023 
-    and coalesce(Act_det.responding, 0) = 0;
+    NumRespondingPersonnelScene
+FROM 
+    Inc_main_extended Inc_main
+WHERE 
+    YEAR(Inc_main.alm_date) >= 2020 
+    AND YEAR(Inc_main.alm_date) <= 2023 
+    AND NumRespondingPersonnelScene = 0;
 
 -- by time of day:
 
-select 
-    hour(alm_time),
-    count(*) as Num_calls 
-from 
-    Inc_main 
-where 
-    year(alm_date) >= 2020 
-    and year(alm_date) <= 2023
-group by 1
-order by 1;
+SELECT  
+    HOUR(alm_time),
+    count(*) AS Num_calls 
+FROM 
+    Inc_main_extended Inc_main 
+WHERE 
+    YEAR(alm_date) >= 2020 
+    AND YEAR(alm_date) <= 2023
+GROUP BY 1
+ORDER BY 1;
+
+--
+
+CREATE TABLE Act_det_extended AS
+SELECT
+    Act_det.*,
+    REGEXP_EXTRACT(Stf_main.Last, '[A-Za-z]+') AS LastName
+FROM
+    Act_det
+LEFT JOIN 
+    Stf_main
+    ON Act_det.Staff_id = Stf_main.Staff_id
+;
 
 -- top responders within year:
 
-select 
-    regexp_extract(Stf_main.Last, '[A-Za-z]+') as LastName,
-    count(*) as NumCalls,
-    (round(count(*)/x.year_count*1000)/10)::VARCHAR || '%' as PercentageCalls
-from Inc_main
-left join 
-    Act_main 
-    on Inc_main.Inci_no = Act_main.Inci_no
-left join 
-    Act_det 
-    on Act_main.Activ_id = Act_det.Activ_id
-left join 
-    Stf_main
-    on Act_det.Staff_id = Stf_main.Staff_id    
-left join 
+SELECT  
+    Act_det.LastName,
+    COUNT(*) AS NumCalls,
+    (ROUND(COUNT(*)/x.year_count*1000)/10)::VARCHAR || '%' AS PercentageCalls
+FROM 
+    Inc_main_extended Inc_main
+LEFT JOIN 
+    Act_det_extended Act_det 
+    ON Inc_main.Activ_id = Act_det.Activ_id  
+LEFT JOIN
     (
-        select 
-            year(Inc_main.alm_date) as year_num, 
-            count(*) as year_count 
-        from 
+        SELECT  
+            YEAR(Inc_main.alm_date) AS year_num, 
+            COUNT(*) AS year_count 
+        FROM 
             Inc_main
-        group by 1
+        GROUP BY 1
     ) x
-    on year(Inc_main.alm_date) = x.year_num
-where 
-    year(Inc_main.alm_date) == 2021
-    -- and (Act_det.Unit != 'STAT' or Act_det.Unit is null)
-group by 
+    ON YEAR(Inc_main.alm_date) = x.year_num
+WHERE 
+    YEAR(Inc_main.alm_date) == 2021
+    -- AND (Act_det.Unit != 'STAT' or Act_det.Unit is null)
+GROUP BY 
     1, x.year_count
-order by 
-    2 desc;
+ORDER BY 
+    2 DESC;
 
 -- all time
 
-select 
-    regexp_extract(Stf_main.Last, '[A-Za-z]+'),
-    count(*) as Num_calls
-from 
-    Inc_main
-left join 
-    Act_main 
-    on Inc_main.Inci_no = Act_main.Inci_no
-left join
-    Act_det 
-    on Act_main.Activ_id = Act_det.Activ_id
-left join 
-    Stf_main
-    on Act_det.Staff_id = Stf_main.Staff_id
-group by 1
-order by 2 desc;
+SELECT  
+    Act_det.LastName,
+    COUNT(*) AS Num_calls
+FROM 
+    Inc_main_extended Inc_main
+LEFT JOIN
+    Act_det_extended Act_det 
+    ON Inc_main.Activ_id = Act_det.Activ_id
+GROUP BY 1
+ORDER BY 2 DESC;
 
 -- first call
 
-select 
-    regexp_extract(Stf_main.Last, '[A-Za-z]+'),
-    min(Inc_main.Alm_date)
-from
-    Inc_main
-left join 
-    Act_main 
-    on Inc_main.Inci_no = Act_main.Inci_no
-left join 
-    Act_det 
-    on Act_main.Activ_id = Act_det.Activ_id
-    -- and (Act_det.Unit != 'STAT' or Act_det.Unit is null)
-left join 
-    Stf_main
-    on Act_det.Staff_id = Stf_main.Staff_id    
-group by 1
-order by 2 desc;
+SELECT  
+    LastName,
+    MIN(Inc_main.Alm_date) AS First_Call
+FROM
+    Inc_main_extended Inc_main
+LEFT JOIN
+    Act_det_extended Act_det 
+    ON Inc_main.Activ_id = Act_det.Activ_id
+    -- AND (Act_det.Unit != 'STAT' or Act_det.Unit is null)
+GROUP BY 1
+ORDER BY 2 DESC;
+
+-- add on first call
+
+CREATE TABLE Act_det_extended_firstcall AS
+SELECT
+    Act_det.*,
+    FirstCall.First_Call
+FROM
+    Act_det
+LEFT JOIN 
+    (
+        SELECT  
+            Act_det.Staff_id,
+            MIN(Inc_main.Alm_date) AS First_Call
+        FROM
+            Inc_main_extended Inc_main
+        LEFT JOIN
+            Act_det_extended Act_det 
+            ON Inc_main.Activ_id = Act_det.Activ_id
+        GROUP BY 1
+        ORDER BY 2 DESC
+    ) FirstCall
+    ON FirstCall.Staff_id = Act_det.Staff_id
+;
+
+-- join staff to
+-- all calls
+-- based on alm_date >= first_call
+-- and alm_date <= retire_date
+-- and count the number of matches
+-- so we get a view of all time, but also with a %
+-- of course, this relies on having a good retire_date
